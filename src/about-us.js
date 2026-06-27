776bef1cd8ee076a1934f060064b65d7a0c7c5e8
Ehab Rizk <203845966+erizk05@users.noreply.github.com>
2026-06-27 22:08:04 +0800

    commit

--- src/about-us.js
+++
@@ -1,6 +0,0 @@
-/**
- * CLEVA Commercial EV - About Us Page Controller
- */
-import './common.js';
-
-console.log('CLEVA About Us module initialized.');
--- src/features.js
+++
@@ -1,6 +0,0 @@
-/**
- * CLEVA Commercial EV - Features / B2B Integration Page Controller
- */
-import './common.js';
-
-console.log('CLEVA B2B Features module initialized.');
